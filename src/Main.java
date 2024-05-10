6eaab3ffc1d41f8c3d83d1cfd0a5a728fff88f3e
VladBliznets <99480189+VladBliznets@users.noreply.github.com>
2024-05-10 08:00:29 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,22 @@
-import java.util.HashSet;
-import java.util.Scanner;
-import java.io.File;
-import java.util.Set;
-import java.util.List;
-import java.util.ArrayList;
 import parcs.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         task curtask = new task();
-        curtask.addJarFile("SubsetGenerator.jar");
+        curtask.addJarFile("QuickSort.jar");
 
-        Set<Integer> set = fromFile(curtask.findFile("input"));
-        List<Set<Integer>> parts = splitSet(set, 4);  // Делим множество на 4 части
+        int[] array = {10, 7, 8, 9, 1, 5};
+        point p = new point(curtask, 0);
+        channel c = p.createChannel();
+        p.execute("QuickSortAM");
+        c.write(array);
 
-        List<point> points = new ArrayList<>();
-        List<channel> channels = new ArrayList<>();
-
-        for (Set<Integer> part : parts) {
-            System.out.println("Part size: " + part.size() + " Elements: " + part); // Только для отладки
-            point p = new point(curtask, 0);
-            channel c = p.createChannel();
-            p.execute("SubsetGenerator");
-            c.write(new SerializableSet(part));
-            points.add(p);
-            channels.add(c);
+        int[] sortedArray = (int[]) c.readObject();
+        System.out.println("Sorted array: ");
+        for (int value : sortedArray) {
+            System.out.print(value + " ");
         }
-
-        
-
-        List<Set<Integer>> allSubsets = new ArrayList<>();
-        for (channel c : channels) {
-            SerializableSubsets receivedSubsets = (SerializableSubsets) c.readObject();
-            allSubsets.addAll(receivedSubsets.getSubsets());
-        }
-
-        System.out.println("Received all subsets: " + allSubsets);
+        System.out.println();
         curtask.end();
     }
-
-    public static Set<Integer> fromFile(String filename) throws Exception {
-        Scanner sc = new Scanner(new File(filename));
-        Set<Integer> set = new HashSet<>();
-        while (sc.hasNextInt()) {
-            set.add(sc.nextInt());
-        }
-        sc.close();
-        return set;
-    }
-
-    private static List<Set<Integer>> splitSet(Set<Integer> originalSet, int partsCount) {
-    List<Set<Integer>> parts = new ArrayList<>();
-    int totalElements = originalSet.size();
-    int partSize = totalElements / partsCount;
-    int remainingElements = totalElements % partsCount;
-    List<Integer> elements = new ArrayList<>(originalSet);
-
-    int start = 0;
-    for (int i = 0; i < partsCount; i++) {
-        int size = partSize + (i < remainingElements ? 1 : 0);
-        Set<Integer> part = new HashSet<>(elements.subList(start, start + size));
-        parts.add(part);
-        start += size;
-    }
-    return parts;
-}
-
 }
