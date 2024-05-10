b01b2caac1b3fbade08cfff542c08bd4eae1121e
VladBliznets <99480189+VladBliznets@users.noreply.github.com>
2024-05-10 06:32:25 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,34 @@
+import parcs.*;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.List;
-import parcs.*;
+import java.util.Scanner;
+import java.io.File;
 
-public static Set<Integer> fromFile(String filename) throws Exception {
-    Scanner sc = new Scanner(new File(filename));
-    Set<Integer> set = new HashSet<>();
-    while (sc.hasNextInt()) {
-        set.add(sc.nextInt());
+public class Main {
+    public static Set<Integer> fromFile(String filename) throws Exception {
+        Scanner sc = new Scanner(new File(filename));
+        Set<Integer> set = new HashSet<>();
+        while (sc.hasNextInt()) {
+            set.add(sc.nextInt());
+        }
+        sc.close();
+        return set;
     }
-    sc.close();
-    return set;
-}
 
-public static void main(String[] args) throws Exception {
-    task curtask = new task();
-    curtask.addJarFile("SubsetGenerator.jar");
+    public static void main(String[] args) throws Exception {
+        task curtask = new task();
+        curtask.addJarFile("SubsetGenerator.jar");
 
-    Set<Integer> set = fromFile(curtask.findFile("input"));
+        Set<Integer> set = fromFile(curtask.findFile("input"));
 
-    point p = new point(curtask, 0);
-    channel c = p.createChannel();
-    p.execute("SubsetGenerator");
-    c.write(set);
+        point p = new point(curtask, 0);
+        channel c = p.createChannel();
+        p.execute("SubsetGenerator");
+        c.write(set);
 
-    List<Set<Integer>> subsets = (List<Set<Integer>>) c.readObject();
-    System.out.println("Received subsets: " + subsets);
-    curtask.end();
+        List<Set<Integer>> subsets = (List<Set<Integer>>) c.readObject();
+        System.out.println("Received subsets: " + subsets);
+        curtask.end();
+    }
 }
