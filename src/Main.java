0c5d617dfe36da0006ad973fefa62689256f67ab
VladBliznets <99480189+VladBliznets@users.noreply.github.com>
2024-05-10 07:39:43 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,40 @@
-import java.util.Set;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.io.File;
+import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
 import parcs.*;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        task curtask = new task();
+        curtask.addJarFile("SubsetGenerator.jar");
+
+        Set<Integer> set = fromFile(curtask.findFile("input"));
+
+        List<point> points = new ArrayList<>();
+        List<channel> channels = new ArrayList<>();
+
+        for (Integer element : set) {
+            point p = new point(curtask, 0);
+            channel c = p.createChannel();
+            p.execute("SubsetGenerator");
+            c.write(new SerializableSet(new HashSet<>(Set.of(element))));
+            points.add(p);
+            channels.add(c);
+        }
+
+        List<Set<Integer>> allSubsets = new ArrayList<>();
+        for (channel c : channels) {
+            SerializableSubsets receivedSubsets = (SerializableSubsets) c.readObject();
+            allSubsets.addAll(receivedSubsets.getSubsets());
+        }
+
+        System.out.println("Received all subsets: " + allSubsets);
+        curtask.end();
+    }
+
     public static Set<Integer> fromFile(String filename) throws Exception {
         Scanner sc = new Scanner(new File(filename));
         Set<Integer> set = new HashSet<>();
@@ -13,22 +43,5 @@ public class Main {
         }
         sc.close();
         return set;
-    }
-
-    public static void main(String[] args) throws Exception {
-        task curtask = new task();
-        curtask.addJarFile("SubsetGenerator.jar");
-
-        Set<Integer> set = fromFile(curtask.findFile("input"));
-        SerializableSet serializableSet = new SerializableSet(set);  // Создаем сериализуемый объект
-
-        point p = new point(curtask, 0);
-        channel c = p.createChannel();
-        p.execute("SubsetGenerator");
-        c.write(serializableSet);  // Передаем сериализуемый объект
-
-        SerializableSubsets receivedSubsets = (SerializableSubsets) c.readObject();
-        System.out.println("Received subsets: " + receivedSubsets.getSubsets());
-        curtask.end();
     }
 }
